d85d019b9f42fc9c412137d1c3ecad96921e0b88
zhuob <zhuob@onid.oregonstate.edu>
2015-08-02 17:04:02 -0700

    under the null, the relation about correlation seems to be hold, even if two genes are not expressed at the same level.

--- PoissonDataTestSampleCor.R
+++ PoissonDataTestSampleCor.R
@@ -1,158 +1,37 @@
-#  poisson regression statistic correlation VS sample correlation
-#
-
-## HOW IS WALD STATISTIC CALCULATED? ####
-#########################################
-N <- 100
-mu <- rep(c(5,10), each=N/2)
-x <- rep(c(1,0), each=N/2)
-
-y <- rpois(N, mu)
-
-pois.mod <- glm(y~x, family = poisson(link = "log"))
-beta0 <- summary(pois.mod)$coe[1, 1]
-beta1 <- summary(pois.mod)$coe[2, 1]
-
-### calculate the hessian matrix 
-var.beta1 <- sum(exp(beta0 + beta1*x)*x^2)
-var.beta0 <- sum(exp(beta0 + beta1*x))
-cov.beta0beta1 <- sum(exp(beta0+ beta1*x)*x)
-
-hessian <- matrix(c(var.beta0, cov.beta0beta1, cov.beta0beta1, var.beta1), 2, 2)
-## calculate the standard deviation for each parameter. 
-## It's the same as the output of glm function.
-sqrt(diag(solve(hessian)))
-summary(pois.mod)
-  
-cor.pois <- function(n, lambda1, r)  ## generate correlated poisson random variables
-{
-  
-lambda2 <- (1-r^2)/(0.1+r^2)*lambda1 # this makes lambda3 always positive
-lambda3 <- lambda1^2/(r^2*(lambda1+lambda2))-lambda1
-
-cor <- lambda1/(sqrt((lambda1+lambda3)*(lambda1+lambda2)))
-
-  y1 <- rpois(n, lambda1)
-  y2 <- rpois(n, lambda2)
-  y3 <- rpois(n, lambda3)
-  
-  x1 <- y1  + y2
-  x2 <- y1  + y3
-  
-  # the correlation between x1 and x2 
-  # rho <- mu1 /sqrt( (mu1 + mu3)*(mu1 + mu2))
-  #  print(round(rho, 4))
-  return(cbind(x1, x2))
-#  return(c(lambda1, lambda2, lambda3, r))
-}
 
 
-
-num <- 20  # how many samples in each group
-# in this case, gene 1 is not DE, but gene 2 is DE
-# for treatment 1
-
-
-source("SimulateLabData.R")
-
-nsim <- 1000
-test.correlation <- sample.correlation <- c()
-r <- seq(0.1, 0.95, length=50)
-trt <- rep(c(0, 1), each=num)
-
-
-calculate.lambda <- function(lambda1, r)
-{
-  lambda2 <- (1-r^2)/(0.1+r^2)*lambda1 # this makes lambda3 always positive
-  lambda3 <- lambda1^2/(r^2*(lambda1+lambda2))-lambda1
-  return(c(lambda1, lambda2, lambda3, r))
-}
-
-lambda.20 <- matrix(NA, ncol=4, nrow=length(r))
-for ( j in 1:length(r))
-{
-  lambda.20[j, ] <- calculate.lambda(37, r[j])
-  
-}
-
-for( k in 1: length(r))
-{
-  
-  t1 <- t2 <- pois.cor <-  c()
-  
-for ( i in 1:nsim)
-{
-  
-  exp1 <- cor.pois(num, 100, r[k]) # pi1 = 4, p2= 5, rho = 0.2236
-  # for treatment 2
-  exp2 <- cor.pois(num, 37, r[k]) #pi1 = 4, p2= 20 rho = 0.2236
-  
-  poisdata <- rbind(exp1, exp2)
-  pois.cor[i] <- calculate.cor(t(poisdata))[1, 2]
-  
-  trt <- rep(c(0, 1), each=num)
-  pois1 <- glm(poisdata[, 1]~trt, family = poisson(link = "log"))
-  #pois1 <- lm(poisdata[, 1]~trt)
-  t1[i] <- summary(pois1)$coe[2, 3] # wald statistic
-  
-  # pois2 <- lm(poisdata[, 2]~trt)
-  pois2 <- glm(poisdata[, 2]~trt, family = poisson(link = "log"))
-  t2[i] <- summary(pois2)$coe[2, 3] # wald statistic
-  
-}
-  
-sample.correlation[k] <- mean(pois.cor)
-test.correlation[k] <- cor(t1, t2)
-}
-
-plot(sample.correlation, r, pch=20)
-points(sample.correlation, test.correlation, col="red", pch=3)
-
-pois.wald.stat.sample.cor <- data.frame(
-  wald.stat= test.correlation, samp.cor= sample.correlation, true=r
-)
-
-saveRDS(pois.wald.stat.sample.cor, "pois.regression.wald.stat.sample.cor.rds")
-
-
-
-
-## pooled correlation coefficient or separate correlation?
-
-## they look the same
-n <- 100
-lambda <- 100
-r <- 0.5
-delta <- 0
-r1 <- r2 <- c()
-for ( i in 1:100)
-{
-  # case
-  y.t1 <- correlated.poisson(lambda, r, n/2)
-  # control
-  y.t2 <- correlated.poisson(lambda + delta, r, n/2)
-  
-  rm1 <- y.t1[1, ] - mean(y.t1[1, ])
-  rm2 <- y.t1[2, ] - mean(y.t1[2, ])
-  
-  rm3 <- y.t2[1, ] - mean(y.t2[1, ])
-  rm4 <- y.t2[2, ] - mean(y.t2[2, ])
-  
-  ## correlation by pooled data  
-  r1[i] <- cor(c(rm1, rm3), c(rm2, rm4))
-  
-  # correlation calculated by separate groups
-  rho1 <- cor(t(y.t1))[1,2]
-  rho2 <- cor(t(y.t2))[1,2]
-  r2[i] <- mean(c(rho1, rho2))
-}
-
-plot(r1, r2)
-
-
-
-
-
+# ## pooled correlation coefficient or separate correlation?
+# 
+# ## they look the same
+# n <- 100
+# lambda <- 100
+# r <- 0.5
+# delta <- 0
+# r1 <- r2 <- c()
+# for ( i in 1:100)
+# {
+#   # case
+#   y.t1 <- correlated.poisson(lambda, r, n/2)
+#   # control
+#   y.t2 <- correlated.poisson(lambda + delta, r, n/2)
+#   
+#   rm1 <- y.t1[1, ] - mean(y.t1[1, ])
+#   rm2 <- y.t1[2, ] - mean(y.t1[2, ])
+#   
+#   rm3 <- y.t2[1, ] - mean(y.t2[1, ])
+#   rm4 <- y.t2[2, ] - mean(y.t2[2, ])
+#   
+#   ## correlation by pooled data  
+#   r1[i] <- cor(c(rm1, rm3), c(rm2, rm4))
+#   
+#   # correlation calculated by separate groups
+#   rho1 <- cor(t(y.t1))[1,2]
+#   rho2 <- cor(t(y.t2))[1,2]
+#   r2[i] <- mean(c(rho1, rho2))
+# }
+# 
+# plot(r1, r2)
+# 
 
 
 
@@ -299,7 +178,7 @@ nreps <- 100
 rho <- seq(0, 0.99, by = 0.01)
 #set.seed(123)
 delta <- rnorm(length(rho), 0, 0.2*lambda)
-delta <- rep(0, length(rho))
+#delta <- rep(0, length(rho))
 
 test.method <- "Score"
 
@@ -325,4 +204,127 @@ ggplot(data = store.corr1, aes(x=true, y=value) ) +
   geom_point(aes(colour=variable)) + 
   labs(x="true", y="correlation", title=test.method)
  
+
+
+
+
+
+###########################################################################
+#  SIMULATE TWO GENES, BUT HAVING DIFFERENT EXPRESSION LEVELS FOR CONTROL GROUP
+###########################################################################
+
+
+
+## set up parameters for desired correlation
+##-------------------------------------------------------------------------
+# the correlation is based on the fact that, x0, x1 and x2 are independent 
+# poisson, then Y1 = x0 + x1 and Y2 = x0 + x2 are correlated.
+##-------------------------------------------------------------------------
+
+lambda0 <- 100                                    
+param.setup <- function(lambda0, lambda2=seq(1, 10000, by =1), rate=1/10)
+{
+         
+  # lambda0 <- 100      
+  # lambda2=seq(1, 10000, by =1)
+  # rate=20
+  lambda1 <- rexp(length(lambda2), rate)                             # lambda1 is simulated from exponential distribution
+  true.rho <- lambda0/sqrt((lambda0 + lambda1)*(lambda0 + lambda2))  # the true correlation is calculated
+  
+  desired.rho <- seq(0.01, 0.99, by = 0.01)                           # we want these
+ 
+  ids <- c()                                                       
+   for( i in 1:length(desired.rho))
+   {
+     ids[i] <- which.min(abs(rhoc - desired.rho[i]))                 # find the which true correlation is closest to the desired one
+   }
+   true.rho.new <- true.rho[ids]                                         # the true correlations
+   lambda0.new <- lambda0*rep(1, length(desired.rho))                # vector of lambda0
+   lambda1.new <- lambda1[ids]                                       # vector of lambda1
+   lambda2.new <- lambda2[ids]                                       # vector of lambda2
+   param <- data.frame(lambda0 = lambda0.new, lambda1 = lambda1.new,
+                       lambda2 =lambda2.new, true.rho= true.rho.new) # the desired parameter 
+  
+   rm.dup <- param[!duplicated(param),]                               # remove the duplicated rows
+}
+
+## --------------------------------------  simulate the correlated data
+
+correlated.poisson2 <- function(lambda0, lambda1, lambda2, n)        # correlated poisson
+{
+
+  x0 <- rpois(n, lambda0)
+  x1 <- rpois(n, lambda1)
+  x2 <- rpois(n, lambda2)
+  
+  y1 <- x1 + x0
+  y2 <- x2 + x0
+  
+  return(rbind(y1, y2))
+}
+
+
+
+
+# -------------------------------------  calculate the correlation under the null of both genes.
+
+sample.stat.correlation2 <- function(lambda0, lambda1, lambda2, n, nreps)
+{
+  
+  
+  u.stat <- matrix(NA, nrow=nreps, ncol=2)                           #  store the score test statistics
+  rho.sample <- c()                                                  # store the sample correlation
+ 
+   for ( k in 1: nreps)
+  {
+    y.t1 <- correlated.poisson2(lambda0, lambda1, lambda2, n/2)      # treatment 
+    y.t2 <- correlated.poisson2(lambda0, lambda1, lambda2, n/2)      # control, DE in 
+    
+   
+    y <- cbind(y.t1, y.t2)                                           # expression data of two genes
+    rho1 <- cor(t(y.t1))[1, 2]; rho2 <- cor(t(y.t2))[1, 2]           # sample correlation within each group
+    rho.sample[k] <- mean(c(rho1, rho2))                             # average them
+    
+    u.stat[k, ] <- score.stat(y)                                     # the score test statistics
+    
+  }
+  
+  rho.ave <- mean(rho.sample)                                        # the overall mean as the sample mean
+  rho.score <- cor(u.stat)[1, 2]                                     # the correlation between the test stats.
+  rho.list <- list(sample.cor= rho.ave, score.stat.cor = rho.score)
+  
+  return(rho.list)
+}
+
+
+## ----------------------------------------  SIMULATION Study
+
+
+param <- param.setup(lambda0 = 10)                                  # the true correlation is in the 4th column
+param <- as.matrix(param)
+dim.param <- dim(param)[1]                                           # the number of true correlations to be simulated
+
+store.corr <- data.frame(matrix(NA,dim.param, 3))                    # store the three correlations
+colnames(store.corr) <- c("sample", "test.stat", "true")             # give them the names
+ 
+
+for ( i in 1:dim.param)
+{
+  a <- sample.stat.correlation2(param[i, 1], param[i, 2], param[i, 3], n=100, nreps=1000)
+  store.corr[i, ] <- c(a$sample.cor, a$score.stat.cor, param[i, 4])
+}
+ 
+
+library(reshape2)
+store.corr1 <- melt(store.corr, id="true")
+library(ggplot2)
+ggplot(data = store.corr1, aes(x=true, y=value) ) +
+  geom_point(aes(colour=variable)) + 
+  labs(x="true", y="correlation", title=test.method)
+
+#-------------  CONCLUSION --------------------------------
+# The correlation remains 1-1 for all null cases
+
+
+
 
