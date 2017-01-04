a6d1e1c845894edb8090378247be75396c274e4a
zhuob <zhuob@onid.oregonstate.edu>
2017-01-03 23:15:58 -0600

    summarizing simulation results

--- simulation-report.R
+++ simulation-report.R
@@ -1,101 +1,33 @@
-library(MEQLEA)
-library(xtable)
+library(meaca)
 library(reshape2)
-library(ggplot2)
 library(dplyr)
-library(gridExtra)
-library(grid)
+library(ggplot2)
+library(cowplot)
 
-FigurePath <- "/Users/Bin/Google Drive/Study/Thesis/Correlation/EnrichmentAnalysis/Manuscript/Figures/"
-FigurePath <- getwd()
-showcol <- c(1,4:9)
-textsize = c(15,15,15, 15)
-
-cases <- c("a","b", "c", "d", "e")
-DEProb1 <- "0PCT"
-DEProb2 <- "10PCT"
-folder <- "Results/Simulation"
-
-for ( i in 1: length(cases)){
-  
-  case1 <- cases[i]
-
-  d1 <- paste(folder, "/TypeIerror_", case1, "_", DEProb1, ".txt", sep="")
-  print(d1)
-  d2 <- paste(folder, "/TypeIerror_", case1, "_", DEProb2, ".txt", sep="")
-  print(d2)
- 
-  dat1 <- read.table(d1, header=T)
-  dat2 <- read.table(d2, header=T)
-  colnames(dat1) <- colnames(dat2) <- c("MEACA", "MEACA-N", "LM", "MRSGE", "SigPath", "CAMERA-modt",
-                                        "CAMERA-rank", "GSEA", "QuSAGE")
-
-  fig.obj <- ArrangeTypeIerror(dat1[, showcol], dat2[,showcol],case = case1, legend=F)
-  ggsave(paste(FigurePath,"/parallel_", case1, ".eps", sep =""), fig.obj,
-         width = 18, height = 4.5)
-  legend_need <- ArrangeTypeIerror(dat1[, showcol], dat2[, showcol], case=case1, legend=T)
-  ggsave(paste(FigurePath,"/parallel_legend.eps", sep =""), legend_need,
-         width = 20, height = 0.6)
-}
+path <- "/Users/Bin/Google Drive/Study/Thesis/Correlation/EnrichmentAnalysis/Results/Simulation2017/Normal2"
 
 
+fig1 <- plot_fig1(path=path)
+ggsave(paste(path,"/fig1", ".eps", sep =""), fig1, width = 18, height = 20)
 
-### type I error table
-
-alpha <- 0.05
-table_typeIerror <- data.frame(matrix(NA, nrow = 1000, ncol = length(showcol)+1))
-for ( i in 1: length(cases)){
-  
-  case1 <- cases[i]
-  
-  d1 <- read.table(paste(folder, "/TypeIerror_", case1, "_", DEProb1, ".txt", sep=""), header=T)
-  ts1 <- typeIerror_quantile(d1, alpha, showcol)
-  d2 <- read.table(paste(folder, "/TypeIerror_", case1, "_", DEProb2, ".txt", sep=""), header=T)
-  ts2 <- typeIerror_quantile(d2, alpha, showcol)
-  table_typeIerror[2*i-1, -1] <- ts1
-  table_typeIerror[2*i, -1] <- ts2
-  names(table_typeIerror) <- c("case", names(ts1))
-  table_typeIerror[c(2*i-1, 2*i), 1] <- paste(case1, c(DEProb1, DEProb2), sep="")
-  }
-
-table_typeIerror <- table_typeIerror[complete.cases(table_typeIerror), ]
-table_typeIerror$struct <- rep(c("(a)", "(b)","(c)", "(d)", "(e)"), each = 2)
-table_typeIerror$group <- rep(c("A1", "A2"), 5)
-table_typeIerror <- table_typeIerror[order(table_typeIerror[, 10], table_typeIerror[, 9]),]
-table_typeIerror <- table_typeIerror[, c(10, 9, 2:8)]
-print(xtable(table_typeIerror,digits =c(0, 0, rep(3, 8))), include.rownames =F)
-
-test_sig <- function(prop, alpha, n){
-  sd <- sqrt((alpha)*(1-alpha)/n)
-  z <- (prop-alpha)/sd
-  2*pnorm(abs(z), lower.tail = F)
-}
-
-significance <- table_typeIerror
-for(j in 1:(ncol(table_typeIerror)-1) ){
-  for ( i in 1:nrow(table_typeIerror)){
-    significance[i, j+1] <- test_sig(table_typeIerror[i, j+1], alpha, 10000)
-    }
-  }
-
-## Power Simulation -----------------------------------------------------------
-#setwd("/Users/Bin/Google Drive/Study/Thesis/Correlation/EnrichmentAnalysis/Share/Simulation/Simulation20160318/")
-## power for figure
-p1 <- plotPower(folder= folder);
-ggsave(paste(FigurePath, "/powerA1pct.eps", sep = ""), p1, width = 8, height = 5)
-p2 <- plotPower(folder = folder, setup = "A2")
-ggsave(paste(FigurePath, "/powerA2pct.eps", sep = ""), p2, width = 8, height = 5)
-
-folder <- "Fixed1"
-##  power table
-library(xtable)
-t1 <- createPowerTable(folder = folder, showcol =  showcol, setup = "A1", case = "a")
-t2 <- createPowerTable(folder = folder, showcol =  showcol, setup = "A2", case = "a")
-xtable(t1, digits = c(0, 3,3,3, 3, 3))
-xtable(t2, digits = c(0, 3,3,3, 3, 3))
+fig2 <- plot_fig2(path, showcol = 1)
+ggsave(paste(path,"/fig2", ".eps", sep =""), fig2, width = 18, height = 20)
 
 
+table1 <- createEmpiricalPowerTable(path, showcol = c(1, 4:9), setup = "A1", case = "a")
+table2 <- createEmpiricalPowerTable(path, showcol = c(1, 4:9), setup = "A2", case = "a")
 
 
+path <- "/Users/Bin/Google Drive/Study/Thesis/Correlation/EnrichmentAnalysis/Results/Power20161223"
+path <- "/Users/Bin/Google Drive/Study/Thesis/Correlation/EnrichmentAnalysis/oldFiles/Share/Simulation/Simulation20160318/Normal1"
+showcol <- c(1, 4, 5, 6, 8, 9)
+ta <- createEmpiricalPowerTable(path, setup = "A2", case = "a", showcol = showcol)
+tb <- createEmpiricalPowerTable(path, setup = "A2", case = "b", showcol = showcol)
+tc <- createEmpiricalPowerTable(path, setup = "A2", case = "c", showcol = showcol)
+td <- createEmpiricalPowerTable(path, setup = "A2", case = "d", showcol = showcol)
+te <- createEmpiricalPowerTable(path, setup = "A2", case = "e", showcol = showcol)
 
+s <- 1
+table3 <- cbind(ta[, s], tb[, s], tc[, s], td[, s], te[, s])
+table3
 
