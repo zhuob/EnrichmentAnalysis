53b165e355ee3ae3dcb57fe2f5b2feffbe746a36
Bin <zhuob@onid.oregonstate.edu>
2020-06-02 20:49:19 -0700

    restructure code

--- meaca/build-package.R
+++ meaca/build-package.R
@@ -1,13 +1,20 @@
+rm(list = ls())
 library(devtools)
 library(roxygen2)
 # create("meaca")
-setwd("/Users/Bin/Google Drive/Study/Thesis/Correlation/EnrichmentAnalysis/meaca/meacaPackage")
-document()
-check()
-build()
+setwd("meaca")
+devtools::load_all()
+# devtools::test()
+devtools::document()
+devtools::check()
+
+Sys.getenv("PATH")
+Sys.setenv(PATH = paste(Sys.getenv("PATH"), "C:\\Program Files\\MiKTeX 2.9\\miktex\\bin\\x64", sep=.Platform$path.sep))
+devtools::build(manual = TRUE)
+
 setwd("..")
-install("meacaPackage")
-install.packages("/Users/Bin/Google Drive/Study/Thesis/Correlation/EnrichmentAnalysis/meaca/meaca_0.0.0.9000.tar.gz", repos = NULL, type="source")
+install("meaca")
+install.packages("meaca/meaca_0.0.0.9000.tar.gz", repos = NULL, type="source")
  
 pack <- "meaca"
 path <- find.package(pack)
