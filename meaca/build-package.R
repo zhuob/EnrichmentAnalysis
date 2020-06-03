d1ab62f9a030e28b00e71cd3361951153096dd85
Bin <zhuob@onid.oregonstate.edu>
2020-06-02 21:48:35 -0700

    re-organize results

--- meaca/build-package.R
+++ meaca/build-package.R
@@ -2,20 +2,20 @@ rm(list = ls())
 library(devtools)
 library(roxygen2)
 # create("meaca")
-setwd("meaca")
 devtools::load_all()
 # devtools::test()
 devtools::document()
 devtools::check()
 
 Sys.getenv("PATH")
-Sys.setenv(PATH = paste(Sys.getenv("PATH"), "C:\\Program Files\\MiKTeX 2.9\\miktex\\bin\\x64", sep=.Platform$path.sep))
 devtools::build(manual = TRUE)
 
-setwd("..")
-install("meaca")
-install.packages("meaca/meaca_0.0.0.9000.tar.gz", repos = NULL, type="source")
+# devtools::install_local("../meaca_0.1.1.tar.gz", dependencies = NA, upgrade = "never")
+install.packages("../meaca_0.1.1.tar.gz", repos = NULL, type = "source")
  
 pack <- "meaca"
 path <- find.package(pack)
-system(paste(shQuote(file.path(R.home("bin"), "R")), "CMD", "Rd2pdf", shQuote(path)))
+if(file.exists(paste0(pack, ".pdf"))){
+  file.remove(paste0(pack, ".pdf"))
+}
+system(paste(file.path(R.home("bin"), "R"), "CMD", "Rd2pdf", shQuote(path)))
