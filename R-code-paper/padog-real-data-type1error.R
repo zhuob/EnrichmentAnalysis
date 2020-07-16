f5b69f5abb2ba6c7a306744582df784d766305a1
bzhuo <rickyb.zh@gmail.com>
2020-07-16 16:21:04 -0700

    upload other packages used in the paper

--- R-code-paper/padog-real-data-type1error.R
+++ R-code-paper/padog-real-data-type1error.R
@@ -18,11 +18,13 @@ alpha_meaca <- function(expression_data, trt, go_term, standardize = TRUE,
   est_sigma <- meaca:::estimate_sigma(expression_data, trt)
   sigma <- est_sigma$sigma
   t_val0 <- est_sigma$t_val
+  rm(est_sigma)
   
   test_genes <- which(go_term == 1)
-  t_test_set <- t_val0[test_genes]
+  back_genes <- which(go_term == 0)
+  # t_test_set <- t_val0[test_genes]
   n_back_set <- sum(go_term == 0)
-  
+  n_test_set <- sum(go_term == 1)
   
   # perform resample of gene level statistics 
   p_alpha <- NULL
@@ -30,12 +32,43 @@ alpha_meaca <- function(expression_data, trt, go_term, standardize = TRUE,
   system.time(
   for(kk in 1:nrep){
     t_val <- t_val0
-    t_val[-test_genes] <- base::sample(t_test_set, size = n_back_set, replace = TRUE)
+    # sample from test set 
+    resample_test_genes <- base::sample(test_genes, size = n_test_set, replace = TRUE)
+    resample_back_genes <- base::sample(test_genes, size = n_back_set, replace = TRUE)
+    t_val[test_genes] <- t_val0[resample_test_genes]
+    t_val[back_genes] <- t_val0[resample_back_genes]
+    
+    # sample the corresponding correlation structures
+    # the new sigma = [q1,   q2]
+    #                 [q2^T, q4]
+    # this form is tested by simple example, should be correct: try this one
+    # ab1 <- matrix(rnorm(24), 6, 4)
+    # ab2 <- cor(t(ab1))
+    # 
+    # kk1 <- c(1,2)
+    # kk2 <- c(4, 3, 6, 5)
+    # ab3 <- ab1[c(kk1, kk2), ]
+    # ab4 <- cor(t(ab3))
+    # q1 <- ab2[kk1, kk1]
+    # q2 <- ab2[kk1, kk2]
+    # q3 <- ab2[kk2, kk1]
+    # q4 <- ab2[kk2, kk2]
+    # ab5 <- rbind(cbind(q1, q2), cbind(q3, q4))
+    # all.equal(ab4, ab5)
+    q1 <- sigma[resample_test_genes, resample_test_genes]
+    q2 <- sigma[resample_test_genes, resample_back_genes]
+    q3 <- sigma[resample_back_genes, resample_test_genes]
+    # q4 <- sigma[resample_back_genes, resample_back_genes]
+    # sigma_new <- rbind(cbind(q1, q2), cbind(q3, q4))
+    
     beta0 <- mean(t_val)
     ones <- rep(1, length(t_val))
     numer <- (crossprod(go_term, (t_val - beta0 * ones)))^2
     go_bar <- rep(mean(go_term), length(go_term))
-    denom2 <- t(go_term - go_bar) %*% sigma %*% (go_term - go_bar)
+    denom2 <- t(go_term - go_bar) %*% 
+              rbind(cbind(q1, q2), cbind(q3, sigma[resample_back_genes, resample_back_genes])) %*% 
+              (go_term - go_bar)
+    
     test_stat <- drop(numer/denom2)
     pval <- 1 - pchisq(test_stat, 1)
     t_temp <- mean(t_val[go_term == 1]) - mean(t_val[go_term == 0])
@@ -65,19 +98,24 @@ alpha_mrgse_sigpath <- function(expression_data, trt, go_term,
   stat <- fit$t[, 2]            							      
   alternative <- "either"                            
   # which genes are in GOTERM
-  index1 <- which(go_term==1)
+  test_genes <- which(go_term == 1)
+  back_genes <- which(go_term == 0)
   n_back_set <- sum(go_term == 0)
-
+  n_test_set <- sum(go_term == 1)
+  
   df_pval <- matrix(nrow = nrep, ncol = 2)
   set.seed(sim_seed)
   for(i in 1:nrep){
     stat_permute <- stat
-    stat_permute[-index1] <- base::sample(stat[index1], size = n_back_set, replace = TRUE)
+    resample_test_genes <- base::sample(test_genes, size = n_test_set, replace = TRUE)
+    resample_back_genes <- base::sample(test_genes, size = n_back_set, replace = TRUE)
+    stat_permute[test_genes] <- stat[resample_test_genes]
+    stat_permute[back_genes] <- stat[resample_back_genes]
     # MRGSE
-    p1 <- limma::geneSetTest(index = index1, stat_permute, 
+    p1 <- limma::geneSetTest(index = test_genes, stat_permute, 
                              alternative = alternative, ranks.only= T)         
     # sigPathway methods
-    p2 <- sig_path(index = index1, stat_permute, nsim = 999)                                                
+    p2 <- sig_path(index = test_genes, stat_permute, nsim = 999)                                                
     df_pval[i, ] <- c(p1, p2)
   }
   names(df_pval) <- c("p_mrgse", "p_sigpath")
@@ -105,15 +143,22 @@ alpha_ora <- function(expression_data, trt, go_term, method = "BH",
   fit_result0 <- limma::topTable(fit, number = length(fit$p.value), 
                                 sort.by = "none", adjust.method = method)
   
+  # identify which genes are from which group
   test_genes <- which(go_term == 1)
+  back_genes <- which(go_term == 0)
   n_back_set <- sum(go_term == 0)
+  n_test_set <- sum(go_term == 1)
   
   p_ora <- NULL
   set.seed(sim_seed)
   for(kk in 1:nrep){
-    index0 <- base::sample(test_genes, size = n_back_set, replace = TRUE)
     fit_result <- fit_result0
-    fit_result[-test_genes, ] <- slice(fit_result0, index0)
+    
+    resample_test_genes <- base::sample(test_genes, size = n_test_set, replace = TRUE)
+    resample_back_genes <- base::sample(test_genes, size = n_back_set, replace = TRUE)
+    fit_result[test_genes, ] <- slice(fit_result0, resample_test_genes)
+    fit_result[back_genes, ] <- slice(fit_result0, resample_back_genes)
+    
     fit_result <- fit_result %>% mutate(adj.P.Val = p.adjust(adj.P.Val, method = method))
     
     fold_change <- abs(fit_result$logFC)  ## the absolute fold change
@@ -168,7 +213,7 @@ alpha_ora <- function(expression_data, trt, go_term, method = "BH",
 # p_gsea <- GSEA.SingleSet(data = expression_data, trt = trt, go_term = go_term, nperm = 1000)$p.vals		
 
 alpha_simu <- function(expression_data, trt, go_term, standardize = TRUE,
-                       nrep = 1e3, sim_seed = 123, method = "BH", thresh = 0.1){
+                       nrep = 1e3, sim_seed = 123, method = "BH", thresh = 0.01){
   
   m1 <- alpha_meaca(expression_data, trt, go_term, standardize = standardize, 
                     nrep = nrep, sim_seed = sim_seed)
@@ -196,8 +241,8 @@ expression_data <- df1$data; trt <- df1$trt; go_term <- df1$go_term
 system.time(result <- alpha_simu(expression_data = expression_data, trt = trt, 
                                  go_term = go_term, standardize = TRUE, 
                                  nrep = 1e3, sim_seed = 123, 
-                                 method = "BH", thresh = 0.1))
+                                 method = "BH", thresh = 0.01))
 
 
-write_csv(result, "padog-real-data-type1erro-simulation.csv")
+write_csv(result, "padog-real-data-type1error-simulation-v2.csv")
 
