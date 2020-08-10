4287d15b0e65e04cb334eea2ffb4252f29a74d0b
bzhuo <rickyb.zh@gmail.com>
2020-08-10 09:23:31 -0700

    code updated for simulation

--- R-code-paper/run-simulation.R
+++ R-code-paper/run-simulation.R
@@ -1,7 +1,9 @@
 library(tidyverse)
 source("R-code-paper/sim-wrapper.R")
 
-use_r_file <- c("R-code-paper/compare-methods.R")
+use_r_file <- c("R-code-paper/compare-methods.R",
+                "R-code-paper/other-methods.R", 
+                "R-code-paper/GSEA.1.0.R")
 
 case_a <- c(0,      0,     0)
 case_b <- c(0.1,  0.1,   0.1)
@@ -9,19 +11,19 @@ case_c <- c(0.1,    0,     0)
 case_d <- c(0.1, 0.05,     0)
 case_e <- c(0.1, 0.05, -0.05)
 
-run_case <- case_b
+run_case <- case_d
 
 # additional simulations: small sample size 
-t1 <- data_simu(nsim = 100,
+t1 <- data_simu(nsim = 1000,
                 ncore = 3,   
                 package_used = c("MASS", "qusage"), 
-                verbose_show = TRUE, 
+                verbose_show = FALSE, 
                 meaca_only = FALSE,
                 file_to_source = use_r_file,
                 dest = "simulation-results/gene-500/n10vs10/", 
                 n_gene = 500,
                 n_test = 100,
-                prop = c(0.0, 0.0),
+                prop = c(0.1, 0.1),
                 rho1 = run_case[1], 
                 rho2 = run_case[2], 
                 rho3 = run_case[3],
