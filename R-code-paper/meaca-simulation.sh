37e79a65b4a2a06cabb45bef0ae1278e04cac015
Bin Zhuo <rickyb.zh@gmail.com>
2020-08-10 20:15:16 -0700

    begin simulation for small sample size

---
+++ R-code-paper/meaca-simulation.sh
@@ -0,0 +1,44 @@
+#!/bin/sh
+
+# Give the job a name
+#$ -N simulation
+
+#$ -S /bin/sh
+
+# set working directory on all host to
+# directory where the job was started
+#$ -cwd
+
+# send output to job.log (STDOUT + STDERR)
+# #$ -o Enrichment_testStatistic.txt
+# #$ -o SimulationLot.txt
+# #$ -o emprical_power.txt
+#$ -o meacaResult_test.txt
+#$ -j y
+
+
+
+# to request 100G free memory
+#$ -l mem_free=100G
+
+
+# email information
+#$ -m e
+# Just change the email address.  You will be emailed when the job has finished.
+#$ -M rickyb.zh@gmail.com
+
+#don't change the source
+source /etc/profile.d/modules.sh
+
+# USE STATISTICS DEPARTMENT CLUSTER
+# #$ -q di.q@di003 
+#$ -q all.q@cosine003
+
+#Change which version of R you want to load on the Compute Nodes
+module load R/3.6.3
+module unload gcc/5.1.0
+module load gcc/7.3.0
+
+# command to run.  ONLY CHANGE THE NAME OF YOUR APPLICATION  
+# Rscript real_data_correlation_simulation.R
+Rscript run-simulation.R
--- R-code-paper/run-simulation.R
+++ R-code-paper/run-simulation.R
@@ -1,9 +1,19 @@
 library(tidyverse)
-source("R-code-paper/sim-wrapper.R")
+parent_folder <- "/home/stats/zhuob/EnrichmentAnalysis/"
+source_r_file <- "R-code-paper/sim-wrapper.R"
+dest <- "simulation-results/gene-500/n10vs10/"
+
+source_r_file <- paste0(parent_folder, source_r_file)
+
+
+source(source_r_file)
 
 use_r_file <- c("R-code-paper/compare-methods.R",
                 "R-code-paper/other-methods.R", 
                 "R-code-paper/GSEA.1.0.R")
+
+use_r_file <- paste0(parent_folder, use_r_file)
+dest <- paste0(parent_folder, dest)
 
 case_a <- c(0,      0,     0)
 case_b <- c(0.1,  0.1,   0.1)
@@ -11,23 +21,23 @@ case_c <- c(0.1,    0,     0)
 case_d <- c(0.1, 0.05,     0)
 case_e <- c(0.1, 0.05, -0.05)
 
-run_case <- case_d
+run_case <- case_e
 
 # additional simulations: small sample size 
-t1 <- data_simu(nsim = 1000,
-                ncore = 3,   
+t1 <- data_simu(nsim = 10000,
+                ncore = 40,   
                 package_used = c("MASS", "qusage"), 
                 verbose_show = FALSE, 
                 meaca_only = FALSE,
                 file_to_source = use_r_file,
-                dest = "simulation-results/gene-500/n10vs10/", 
+                dest = dest, 
                 n_gene = 500,
                 n_test = 100,
-                prop = c(0.1, 0.1),
+                prop = c(0.0, 0.0),
                 rho1 = run_case[1], 
                 rho2 = run_case[2], 
                 rho3 = run_case[3],
-                size = 50,
+                size = 20,
                 de_mu = 2,
                 de_sd = 1,
                 data_gen_method = "chol",
